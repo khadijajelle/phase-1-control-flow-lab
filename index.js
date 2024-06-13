198e34169fb69d1a31a0788142725c715502efb7
khadija <khadijajelle3@gmail.com>
2024-06-13 12:22:14 +0300

    complete control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(somevalue){
+  if (somevalue <=400){
+    return  'This one is on me!';
+  }
+  else if (somevalue >  2000 && somevalue<= 2500 ){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (somevalue > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good.":'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case  'generous':
+    return  'Thank you so much.';
+    case 'not as generous':
+      return  "Thank you.";
+      default:
+        return  'Bye.';
+    
+  }
+  
 }
